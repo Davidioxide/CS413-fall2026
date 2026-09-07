c84a7d6cde33119cdf0802283438af365dda776d
DaveF <D3188667166@outlook.com>
2026-09-07 17:43:38 -0400

    Fix discount calculator

--- lab1/discount_calculator.py
+++ lab1/discount_calculator.py
@@ -4,7 +4,7 @@
 def calculate_total(prices, discount_percent):
     """Return the total after applying a percentage discount."""
     subtotal = sum(prices)
-    return subtotal - discount_percent
+    return subtotal - subtotal * discount_percent / 100
 
 
 def main():
